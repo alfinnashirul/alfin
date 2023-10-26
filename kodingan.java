b6d06b579891b924f545cfa6d8c0eb56ce7408b6
alfinnashirul <alfin080704@gmail.com>
2023-10-26 08:43:47 -0700

    kodingan

--- kodingan.java
+++ kodingan.java
@@ -1,6 +1,6 @@
 public class kodingan {
     
     public static void main(String[] args) {
-        System.out.println("apa saja ganti");
+        System.out.println("apa saja perubahan");
     }
 }
